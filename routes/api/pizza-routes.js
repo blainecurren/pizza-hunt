87095c921b556bfb9363dace2b582af54ede7063
Blaine Curren <blaine.curren@gmail.com>
2022-11-29 13:47:14 -0600

    updated routes/index.js

--- routes/api/pizza-routes.js
+++ routes/api/pizza-routes.js
@@ -1,10 +1,16 @@
 const router = require("express").Router();
+const {
+  getAllPizza,
+  getPizzaById,
+  createPizza,
+  updatePizza,
+  deletePizza,
+} = require("../../controllers/pizza-controller");
 
 // set GET all and POST at /api/pizzas
-router.route("/").get().post();
+router.route("/").get(getAllPizza).post(createPizza);
 
 // set up GET one, PUT, and DELETE at /api/pizzas/:id
-router.route("/:id").get().put().delete();
+router.route("/:id").get(getPizzaById).put(updatePizza).delete(deletePizza);
 
 module.exports = router;
-
