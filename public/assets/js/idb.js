28b3d5f2a2cdff35cedfa19fae28cb0c5d9a342f
Blaine Curren <blaine.curren@gmail.com>
2022-12-04 00:24:14 -0600

    established indexedDB connection

---
+++ public/assets/js/idb.js
@@ -0,0 +1,25 @@
+let db;
+
+const request = indexedDB.open("pizza-hunt", 1);
+
+// this event will emit if the database version changes
+request.onupgradeneeded = function (event) {
+  // save a reference to the database
+  const db = event.target.result;
+  // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts
+  db.createObjectStore("new_pizza", { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+  // when db is successfully created with its object store
+  db = event.target.result;
+
+  // check if app is online, if yes run uploadPizza() function to send all local db data to api
+  if (navigator.onLine) {
+    // uploadPizza();
+  }
+};
+
+request.onerror = function (event) {
+  console.log(event.target.errorCode);
+};
